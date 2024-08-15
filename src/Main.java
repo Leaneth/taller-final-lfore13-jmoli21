6069f17d7442ec70cb86a1f1762d14e51b9f9c88
Leaneth <leaforerob@gmail.com>
2024-08-15 10:08:28 -0500

    Modificación al Menu Principal

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
 
+        int opcion;
+
+        System.out.println("Digita la opción deseada: ");
+
+        Scanner scannerMenu = new Scanner(System.in);
+
+
+
+  do{
         System.out.println("-------------------------------------------------");
         System.out.println("-        MENU CAPÍTULO DE PROGRAMACIÓN          -");
         System.out.println("-      GABRIELA MOLINA - LEANETH FORERO         -");
@@ -22,82 +31,78 @@ public class Main {
         System.out.println("-          0.  Salir                            -");
         System.out.println("-------------------------------------------------");
 
+      opcion = scannerMenu.nextInt();
 
-        int opcion;
-
-        System.out.println("Digita la opción deseada: ");
-
-        Scanner scannerMenu = new Scanner(System.in);
-
-        opcion = scannerMenu.nextInt();
 
         switch (opcion) {
 
             case 1:
                 System.out.println("-------------------------------------------------");
-                System.out.println("                 DATOS PRIMITIVOS                ");
+                System.out.println("-               DATOS PRIMITIVOS                -");
                 System.out.println("-------------------------------------------------");
                 break;
             case 2:
                 System.out.println("-------------------------------------------------");
-                System.out.println("                      STRING                     ");
+                System.out.println("-                    STRING                     -");
                 System.out.println("-------------------------------------------------");
                 break;
             case 3:
                 System.out.println("-------------------------------------------------");
-                System.out.println("                    CONSTANTES                   ");
+                System.out.println("-                  CONSTANTES                   -");
                 System.out.println("-------------------------------------------------");
                 break;
              case 4:
                 System.out.println("-------------------------------------------------");
-                System.out.println("               TIPOS DE OPERADORES               ");
+                System.out.println("-              TIPOS DE OPERADORES              -");
                 System.out.println("-------------------------------------------------");
                  break;
             case 5:
                 System.out.println("-------------------------------------------------");
-                System.out.println("               IF - ELSE -  IF ELSE              ");
+                System.out.println("-             IF - ELSE -  IF ELSE              -");
                 System.out.println("-------------------------------------------------");
                 break;
             case 6:
                 System.out.println("-------------------------------------------------");
-                System.out.println("                      SWITCH                     ");
+                System.out.println("-                    SWITCH                     -");
                 System.out.println("-------------------------------------------------");
                 break;
             case 7:
                 System.out.println("-------------------------------------------------");
-                System.out.println("                      TERNARÍA                   ");
+                System.out.println("-                   TERNARÍA                    -");
                 System.out.println("-------------------------------------------------");
                 break;
             case 8:
                 System.out.println("-------------------------------------------------");
-                System.out.println("                      DO WHILE                   ");
+                System.out.println("-                   DO WHILE                    -");
                 System.out.println("-------------------------------------------------");
                 break;
             case 9:
                 System.out.println("-------------------------------------------------");
-                System.out.println("                      TERNARÍA                   ");
+                System.out.println("-                    TERNARÍA                   -");
                 System.out.println("-------------------------------------------------");
                 break;
             case 10:
                 System.out.println("-------------------------------------------------");
-                System.out.println("                         FOR                     ");
+                System.out.println("-                      FOR                      -");
                 System.out.println("-------------------------------------------------");
                 break;
             case 0:
                 System.out.println("-------------------------------------------------");
-                System.out.println("                        SALIR                    ");
+                System.out.println("-                     SALIR                     -");
                 System.out.println("-------------------------------------------------");
               //xit = true; // Cambia la variable para salir del bucle
                 break;
 
 
             default :
-                System.out.println("- OPCIÓN NO VALIDA- INGRESA UNA OPCIÓN DEL MENÚ -");
+                System.out.println("-------------------------------------------------");
+                System.out.println("-  OPCIÓN NO VALIDA/INGRESA UNA OPCIÓN DEL MENÚ -");
+                System.out.println("-------------------------------------------------");
         }
 
 
-
-
+  }while (opcion!=0);
+        System.out.println("HASTA LUEGO");
     }
 
 }
