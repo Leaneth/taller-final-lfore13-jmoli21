211285bc295528cb5b34a984de3ad00aff111248
Leaneth <leaforerob@gmail.com>
2024-09-18 16:55:58 -0500

    creación de paquetes y clases

--- src/logica/Main.java
+++ src/logica/Main.java
@@ -1,3 +1,8 @@
+package logica;
+
+
+import menus.MenuPrincipal;
+
 import java.util.Scanner;
 public class Main {
 
@@ -19,30 +24,15 @@ public class Main {
         int opcion10;
 
 
-        System.out.println("Digita la opción deseada: ");
+
 
         Scanner scannerMenu = new Scanner(System.in);
 
 
 
   do{
-        System.out.println("-------------------------------------------------");
-        System.out.println("-        MENU CAPÍTULO DE PROGRAMACIÓN          -");
-        System.out.println("-      GABRIELA MOLINA - LEANETH FORERO         -");
-        System.out.println("-            JMOLI21   -   LFORE13              -");
-        System.out.println("-------------------------------------------------");
-        System.out.println("-          1.  Datos Primitivos                 -");
-        System.out.println("-          2.  String                           -");
-        System.out.println("-          3.  Constantes                       -");
-        System.out.println("-          4.  Tipo de Operadores               -");
-        System.out.println("-          5.  Condicional IF -ELSE -IF ELSE    -");
-        System.out.println("-          6.  Condicional Switch               -");
-        System.out.println("-          7.  Condicional Ternaría             -");
-        System.out.println("-          8.  Bucle  DO  WHILE                 -");
-        System.out.println("-          9.  Bucle  WHILE                     -");
-        System.out.println("-          10. Bucle  FOR                       -");
-        System.out.println("-          0.  Salir                            -");
-        System.out.println("-------------------------------------------------");
+       //va menu principal
+      MenuPrincipal.menuprincipal();
 
 
       opcion = scannerMenu.nextInt();
@@ -165,7 +155,7 @@ public class Main {
                     scannerMenu.nextLine();//Ahora el programa se detiene hasta que se pulse ENTER
 
 
-                } while (opcion1 != 0);
+                    } while (opcion1 != 0);
 
                 break;
 
---
+++ src/menus/MenuPrincipal.java
@@ -0,0 +1,31 @@
+package menus;
+
+public class MenuPrincipal {
+
+
+    public static void menuprincipal() {
+
+        System.out.println("-------------------------------------------------");
+        System.out.println("-        MENU CAPÍTULO DE PROGRAMACIÓN          -");
+        System.out.println("-      GABRIELA MOLINA - LEANETH FORERO         -");
+        System.out.println("-            JMOLI21   -   LFORE13              -");
+        System.out.println("-------------------------------------------------");
+        System.out.println("-          1.  Datos Primitivos                 -");
+        System.out.println("-          2.  String                           -");
+        System.out.println("-          3.  Constantes                       -");
+        System.out.println("-          4.  Tipo de Operadores               -");
+        System.out.println("-          5.  Condicional IF -ELSE -IF ELSE    -");
+        System.out.println("-          6.  Condicional Switch               -");
+        System.out.println("-          7.  Condicional Ternaría             -");
+        System.out.println("-          8.  Bucle  DO  WHILE                 -");
+        System.out.println("-          9.  Bucle  WHILE                     -");
+        System.out.println("-          10. Bucle  FOR                       -");
+        System.out.println("-          0.  Salir                            -");
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("Digita la opción deseada: ");
+
+    }
+
+
+}
